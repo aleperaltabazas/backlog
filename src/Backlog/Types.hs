99b3b3e71f7b320d8f81fb4351ad0db28399b287
Alejandro <aleperaltabazas@gmail.com>
2026-05-05 18:53:04 -0300

    feat: add core types

--- src/Backlog/Types.hs
+++ src/Backlog/Types.hs
@@ -1,1 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Backlog.Types where
+
+import Data.Map.Strict (Map)
+import Data.Text (Text)
+
+data Column = Backlog | WIP | Done
+  deriving (Eq, Ord, Show, Enum, Bounded)
+
+data Task = Task
+  { taskSlug        :: Text
+  , taskTitle       :: Text
+  , taskDescription :: Text
+  , taskColumn      :: Column
+  } deriving (Eq, Show)
+
+type Board = Map Column [Task]
+
+data ActiveWidget
+  = BoardWidget
+  | DetailWidget
+  | NewTaskWidget
+  | EditWidget      -- editing an existing task's title (on board) or description (in detail)
+  | ConfirmWidget
+  deriving (Eq, Show)
+
+data ResourceName
+  = TaskListName Column
+  | NewTaskEditorName
+  deriving (Eq, Ord, Show)
+
+columnName :: Column -> Text
+columnName Backlog = "BACKLOG"
+columnName WIP     = "WIP"
+columnName Done    = "DONE"
+
+columnDirName :: Column -> FilePath
+columnDirName Backlog = "backlog"
+columnDirName WIP     = "wip"
+columnDirName Done    = "done"
